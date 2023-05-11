948a132f6a21a48748569cbd85b765fd6d1a4075
AlbertSigsbert <ishengomaalbert@gmail.com>
2023-05-11 15:49:56 +0300

    Displaying Question Summary

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -6,37 +7,44 @@ class ResultScreen extends StatelessWidget {
 
   final List<String> chosenQuestions;
 
-  List<Map<String,Object>> getSummaryData(){
-      final List<Map<String,Object>> summary = [];
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
 
-      for(var i = 0; i<chosenQuestions.length; i++){
-        summary.add({
-          'question_index':i,
-          'question':questions[i].text,
-          'correct_answer':questions[i].answers[0],
-          'user_answer':chosenQuestions[i]
-        });
-      }
+    for (var i = 0; i < chosenQuestions.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenQuestions[i]
+      });
+    }
 
-      return summary;
+    return summary;
   }
 
   @override
   Widget build(context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data){
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
+
     return SizedBox(
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, 
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'You answered 3 out of 6 questions correctly!',
-              style: TextStyle(fontSize: 18, color: Colors.white),
+             Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+              style: const TextStyle(fontSize: 18, color: Colors.white),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
-            const Text('Scrollable Widget...'),
+            QuestionsSummary(summaryData),
             const SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {},
